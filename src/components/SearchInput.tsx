c7667afbd31a42f9a5beabbafa6b961ef8969e98
Sharon Arana <shar_mdza@hotmail.com>
2022-07-11 15:21:29 -0500

    searcher styles added

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/components/SearchInput.styl";
 
 interface SearchInputProps {
   handleChange: (text: string) => void;
@@ -9,14 +10,22 @@ interface SearchInputProps {
 const SearchInput = (props: SearchInputProps) => {
   const { user, handleClick, handleChange } = props;
   return (
-    <div>
-      <input
-        type="text"
-        value={user}
-        onChange={(e) => handleChange(e.target.value)}
-      />
-      <button onClick={handleClick}>Search</button>
-    </div>
+    <>
+      <div className="github-logo">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+          alt="github logo"
+        />
+      </div>
+      <div className="input-searcher">
+        <input
+          type="text"
+          value={user}
+          onChange={(e) => handleChange(e.target.value)}
+        />
+        <button onClick={handleClick}>Search</button>
+      </div>
+    </>
   );
 };
 
